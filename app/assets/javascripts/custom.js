d29a953b7c42ddf626d7458eea5420e2e2db8714
johncox00 <john@mach19.com>
2013-05-15 22:58:25 -0600

    accuate link

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -91,6 +91,35 @@ function process_lightshow(details){
     }
 }
 
+function actuateLink(link)
+{
+   var allowDefaultAction = true;
+      
+   if (link.click)
+   {
+      link.click();
+      return;
+   }
+   else if (document.createEvent)
+   {
+      var e = document.createEvent('MouseEvents');
+      e.initEvent(
+         'click'     // event type
+         ,true      // can bubble?
+         ,true      // cancelable?
+      );
+      allowDefaultAction = link.dispatchEvent(e);           
+   }
+         
+   if (allowDefaultAction)       
+   {
+      var f = document.createElement('form');
+      f.action = link.href;
+      document.body.appendChild(f);
+      f.submit();
+   }
+}
+
 function play_audio(){
     var audio = document.getElementById('audio');
     audio.play();
